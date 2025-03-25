48351ff04c2f30fc8f9892cdf4eaa33807399578
Kamran Ali <ely.kamran@gmail.com>
2025-03-25 08:05:00 -0400

    api bar

---
+++ src/CSharp.ApiRequest.Bar/Startup.cs
@@ -0,0 +1,43 @@
+using CSharp.ApiRequest.Bar.Communications.Interfaces;
+using CSharp.ApiRequest.Library.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CSharp.ApiRequest.Bar
+{
+    public sealed class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(configuration);
+
+            services.AddCommunicationDependencies();
+            services.AddScoped<IApiRequest, Communications.ApiRequest>();
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(routeBuilder => routeBuilder.MapControllers());
+        }
+    }
+}
